b1bd6351dd9ac6b1d889a8c15fd4b3bf09e2ca4a
Alisher Khamidov <a.a.khamidov@yandex.ru>
2026-04-28 13:02:24 +0200

    pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
+import { AdvertModule } from './advert/advert.module';
 
 @Module({
-  imports: [ArticleModule],
+  imports: [ArticleModule, AdvertModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// tight coupling
+// loose coupling
+
+// declarative approach - we just name things - and they exist this way
+// imperative approach - we call of functions - they do smth
+
+// monolith
+// modular monolith
+// microservices
