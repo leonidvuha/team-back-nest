bb512de5210ee77d3b0513dbfb760a9ae1787047
Yuliia Ivanets <yuliya.ivanets@gmail.com>
2026-05-20 10:10:16 +0200

    feat: add Product, Category, Unit enum, Status enum and update Role

---
+++ prisma/migrations/20260520080709_add_product_and_category/migration.sql
@@ -0,0 +1,84 @@
+/*
+  Warnings:
+
+  - The values [FARMER] on the enum `Role` will be removed. If these variants are still used in the database, this will fail.
+  - You are about to drop the column `quantity` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `products` table. All the data in the column will be lost.
+  - You are about to alter the column `price` on the `products` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+  - Added the required column `category_id` to the `products` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `image_url` to the `products` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lat` to the `products` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lng` to the `products` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `owner_id` to the `products` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `unit` on the `products` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Added the required column `full_name` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "product_status" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "unit" AS ENUM ('kg', 'l', 'st');
+
+-- AlterEnum
+BEGIN;
+CREATE TYPE "Role_new" AS ENUM ('USER', 'ADMIN');
+ALTER TABLE "public"."users" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "users" ALTER COLUMN "role" TYPE "Role_new" USING ("role"::text::"Role_new");
+ALTER TYPE "Role" RENAME TO "Role_old";
+ALTER TYPE "Role_new" RENAME TO "Role";
+DROP TYPE "public"."Role_old";
+ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'USER';
+COMMIT;
+
+-- DropForeignKey
+ALTER TABLE "products" DROP CONSTRAINT "products_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "products" DROP COLUMN "quantity",
+DROP COLUMN "userId",
+ADD COLUMN     "category_id" INTEGER NOT NULL,
+ADD COLUMN     "image_url" TEXT NOT NULL,
+ADD COLUMN     "lat" DECIMAL(10,7) NOT NULL,
+ADD COLUMN     "lng" DECIMAL(10,7) NOT NULL,
+ADD COLUMN     "owner_id" TEXT NOT NULL,
+ADD COLUMN     "status" "product_status" NOT NULL DEFAULT 'ACTIVE',
+ADD COLUMN     "tags" TEXT[] DEFAULT ARRAY[]::TEXT[],
+ALTER COLUMN "price" SET DATA TYPE DECIMAL(10,2),
+DROP COLUMN "unit",
+ADD COLUMN     "unit" "unit" NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "avatar_url" TEXT,
+ADD COLUMN     "full_name" TEXT NOT NULL,
+ADD COLUMN     "phone" TEXT;
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_slug_key" ON "categories"("slug");
+
+-- CreateIndex
+CREATE INDEX "products_owner_id_idx" ON "products"("owner_id");
+
+-- CreateIndex
+CREATE INDEX "products_category_id_idx" ON "products"("category_id");
+
+-- CreateIndex
+CREATE INDEX "products_status_idx" ON "products"("status");
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
